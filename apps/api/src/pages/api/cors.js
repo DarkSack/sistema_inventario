c4418c195680df7933cfd6a1623bebe13ec14a7a
DarkSack <johanjafet4@gmail.com>
2024-05-18 21:55:23 -0600

    Update cors.js

--- apps/api/src/pages/api/cors.js
+++ apps/api/src/pages/api/cors.js
@@ -1,6 +1,9 @@
 // src/pages/api/cors.js
 export const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://sackitoinventoryfrontend.vercel.app"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
