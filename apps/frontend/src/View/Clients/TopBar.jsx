8a7b8c465ef41d53116e6941f63969f16c4fb3ba
DarkSack <johanjafet4@gmail.com>
2024-05-21 21:15:39 -0600

    update

--- apps/frontend/src/View/Clients/TopBar.jsx
+++ apps/frontend/src/View/Clients/TopBar.jsx
@@ -1,10 +1,8 @@
 import { Grid, Tabs } from "@mantine/core";
-import { useNavigate } from "react-router-dom";
 import { Product } from "./Product";
 import { useMediaQuery } from "@mantine/hooks";
 
 export const TopBar = () => {
-  const navigate = useNavigate();
   const isMobile = useMediaQuery("(max-width: 569px)")
     ? "vertical"
     : "horizontal";
@@ -20,7 +18,7 @@ export const TopBar = () => {
             <Tabs.Tab value="Help">Help</Tabs.Tab>
             <Tabs.Tab value="Categories">Categories</Tabs.Tab>
             <Tabs.Tab value="About">About</Tabs.Tab>
-            <Tabs.Tab value="LogOut" className="cursor-pointer" onClick={navigate("/logout")}>
+            <Tabs.Tab value="LogOut" className="cursor-pointer">
               Logout
             </Tabs.Tab>
           </Tabs.List>
