c092ccee2854cdf0c56e4f6155b296bf25cd01b3
DarkSack <johanjafet4@gmail.com>
2024-06-18 17:55:55 -0600

    Commit

--- apps/api/next.config.mjs
+++ apps/api/next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_PROJECT_URL: process.env.NEXT_PUBLIC_PROJECT_URL,
-    NEXT_PUBLIC_APIKEY: process.env.NEXT_PUBLIC_APIKEY,
-  },
 };
 
 export default nextConfig;
